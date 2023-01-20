be3af5e8e76a9392a5f698996f3ba3a61b05ec0f
RafallPawlak <104594858+RafallPawlak@users.noreply.github.com>
2023-01-20 17:49:13 +0100

    up

---
+++ src/api/Api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const fetchImages = async (query, page) => {
+  const response = await axios.get(`https://pixabay.com/api/`, {
+    method: 'get',
+    params: {
+      key: '31011594-1ff7c9f95cd2b442eeb7c007c',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 12,
+      page: page,
+    },
+  });
+
+  return response.data;
+};
